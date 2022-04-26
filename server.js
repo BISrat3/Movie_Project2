72d920f5d1f6a65e65b306887e6eb53d23a2cc65
mdechavez5 <marcdechavez@gmail.com>
2022-04-26 17:47:51 -0400

    Added new route & post. Added new.ejs.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
 
 // create instance
 const app = express()
@@ -7,12 +9,38 @@ const app = express()
 // configure the app settings 
 const PORT = 3000
 
+// Models - databases
+const db = require('./models')
+
 // app configs 
 app.set('view engine', 'ejs')
+
+// body-parser middleware
+app.use(express.urlencoded({ extended: false }))
 
 // "Index" route
 app.get('/movies', (req,res) =>{
     res.render('index.ejs')
+})
+
+// "New" route
+app.get('/movies/new', (req,res) => {
+    res.render('new.ejs')
+})
+
+// Post "New" route
+app.post('/movies', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+    // try {
+    //     const createMovie = await db.Movie.create(req.body)
+    //     console.log(createMovie)
+    //     res.redirect('/movies')
+    // } catch (error) {
+    //     console.log(error);
+    //     req.error = error;
+    //     return next();
+    // }
 })
 
 // "Home" route

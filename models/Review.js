279ea465f7ba6e6ec28c57078baf3b4fc0ae0d8b
mdechavez5 <marcdechavez@gmail.com>
2022-04-26 15:56:38 -0400

    Added models directory w/ Movie schema & Review schema

---
+++ models/Review.js
@@ -0,0 +1,28 @@
+// Import Mongoose Dependency
+const mongoose = require('mongoose')
+
+
+// Review Schema
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+    },
+    comment: {
+        type: String,
+        required: [true, "You must provide a reason for your review"]
+    },
+    movie: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Movie',
+    }
+})
+
+// Models Review Schema
+const Review = mongoose.model('Review', reviewSchema)
+
+// Export Review
+module.exports = Review
+

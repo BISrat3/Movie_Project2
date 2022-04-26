7ce99770541decaf9ceae06ff34e6b8ab80f3198
bisrat <bisrat.amtataw@gmail.com>
2022-04-26 15:59:29 -0400

    index_page

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express')
+
 const app = express()
+
 const PORT = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/movies', (req,res) =>{
+    res.render('index.ejs')
+})
 
 app.get('/', (req,res) =>
     res.send("Welcome to Movie Page"))

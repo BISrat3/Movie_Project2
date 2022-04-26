0e919af6817265a8de6ce2d1a55d4388a2566254
bisrat <bisrat.amtataw@gmail.com>
2022-04-26 15:46:26 -0400

    Home Page

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-// port 
+
+app.get('/', (req,res) =>
+    res.send("Welcome to Movie Page"))
+
 app.listen(PORT, ()=>
     console.log(`Listening on port: ${PORT}`)
 )

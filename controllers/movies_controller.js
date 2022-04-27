f46ad7ac02fae710787db6eb9eb85b3ff1bf4502
bisrat <bisrat.amtataw@gmail.com>
2022-04-27 10:50:50 -0400

    review_controller

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -1,7 +1,5 @@
 // import express
 const express = require('express')
-const methodOverride = require('method-override')
-
 
 const router = express.Router()
 
@@ -20,8 +18,6 @@ router.get('/new', (req,res) => {
 
 // Post "New" route
 router.post('/', async (req, res, next) => {
-    // console.log(req.body)
-    // res.send(req.body)
     try {
         const createMovie = await db.Movie.create(req.body)
         console.log(createMovie)

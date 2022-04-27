2c5fe57bee069a70627e9105c2aa02e0a1dd228a
mdechavez5 <marcdechavez@gmail.com>
2022-04-27 11:18:02 -0400

    Working on show.ejs. Added show route to movies controller. Added ejs partials for head & nav.

--- controllers/movies_controller.js
+++ controllers/movies_controller.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express')
+const methodOverride = require('method-override')
+
 
 const router = express.Router()
 
@@ -22,6 +24,21 @@ router.get('/', async (req, res, next) =>{
 // "New" route
 router.get('/new', (req,res) => {
     res.render('new.ejs')
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const foundMovie = await db.Movie.findById(req.params.id)
+        // Need line to find all Reviews here
+        const context = {
+            movie: foundMovie,
+        }
+        res.render('show.ejs', context)
+    } catch (error) {
+        console.log(error);
+        req.error = error;
+        return next();
+    }
 })
 
 // Post "New" route

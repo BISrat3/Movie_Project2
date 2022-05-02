9852b07fdf362fc417eaa4ea4bcf41f351da427b
bisrat <bisrat.amtataw@gmail.com>
2022-05-02 13:38:08 -0400

    delete_consoleLog_and extra comments

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,18 +1,17 @@
+// import express
 const express = require('express')
-
 const bcrypt = require('bcryptjs')
-
 const router = express.Router()
 
 // Models - databases
 const db = require('../models')
 
-// "New" sing In route
+// "New" Sign In route
 router.get('/signin', (req,res) => {
     res.render('users/signin.ejs')
 })
 
-// Post "User" route - Post SignIn route
+// Post "User" route - Post Sign In route
 router.post('/signin', async (req, res) => {
     try {
         // Check if user exists
@@ -26,7 +25,6 @@ router.post('/signin', async (req, res) => {
         }
         return res.redirect('/movies')
     } catch (error) {
-        console.log(error);
         res.send(err)
     }
 })
@@ -41,7 +39,6 @@ router.post('/register', async (req, res, next) => {
     try {
         const foundUser = await db.User.exists({username: req.body.username})
         if (foundUser){
-            // console.log(`You already have an account`)
             return res.redirect('/signin')
         }
         const salt = await bcrypt.genSalt(10)
@@ -50,20 +47,20 @@ router.post('/register', async (req, res, next) => {
         const newUser= await db.User.create(req.body)
         return res.redirect('/signin')
     } catch (error) {
-        console.log(error);
-        req.error = error;
-        return next();
+        req.error = error
+        return next()
     }
 })
 
+// signout route
 router.get("/signout", async (req, res) => {
     try {
-        await req.session.destroy();
-        return res.redirect("/signin");
+        await req.session.destroy()
+        return res.redirect("/signin")
     } catch (error) {
-        console.log(error);
-        return res.send(error);
+        return res.send(error)
     }
 });
 
+// export router
 module.exports = router
